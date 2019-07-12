33b379bc8c190c40c803f5366d88663bc811e9a5
manulaMongo <jainshishir19@gmail.com>
2019-07-12 16:33:42 +0530

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-//
+//sds
 const outputDirectory = 'dist';
 
 module.exports = {
